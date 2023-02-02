51f715cbf72819903a5958439452cc8caa6f488c
Phil Culling <philculling@hotmail.com>
2023-02-02 12:25:07 +0000

    update for loop and if statement to fix color issue; not working yet but saved so code can be pushed prior to tutor meeting

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -66,8 +66,8 @@
   moment().hours(Number);
   moment().hours();
   console.log(moment().hours());
-  console.log(JSON.parse(moment().hour()));//hoping this is right syntax
-  //Both return 24 hour clock which is helpful.
+  console.log(JSON.parse(moment().hour()));//hoping this is right syntax?
+  //All, including the JSON one, return 24 hour clock which is helpful.
  //test code
  if (moment().hour() === 10) {
   console.log("I can use this now")
@@ -75,9 +75,7 @@
  if (moment().hour() < 11) {
   console.log("Now I need to work out how to link this to the data attributes")
  }
-
- var allTextAreas = $('.col-8');//to target all middle columns
- allTextAreas.text("Test");
+//end of test code
 
  $('#container')
 .children('div')
@@ -85,51 +83,36 @@
 .css('border-radius', '.4rem')
 .css('margin', '20px');//that was a test, you will probably want to change it
 
-for (i = 0; i < allTextAreas.length; i++) {
-  allTextAreas.text("Test2");
-  if (id = "9") {
-    var changeEl = $('#9');
-    changeEl.css('background-color', 'red');
+var targetDivs = $('#container').children('div');//not sure here but I think it's ok
+for (i = 0; i < targetDivs.length; i++) {
+  var state = ("data-hour", targetDivs[i]);//not sure here, as...
+  console.log(state);//returning too much, not quite targeted correctly
+  if (state === JSON.parse(moment().hour())) { //check syntax with Matthew
+    $("#container").children[i].addClass("present");
   }
+  if (state < JSON.parse(moment().hour())) {
+    $("#container").children[i].addClass("past");
+  }
+  else (state > JSON.parse(moment().hour()))
+  $("#container").children[i].addClass("future");//uncaught type error
 }
-  /*that worked so loop syntax is ok
-  This needs to be finished to create a loop and inner if
-  statement that iterates through and makes a comparison with the 
-  moment get hour, somehow, by targeting data-hours you've set up.
+
+  /*
+  Pseudo of y intentions:
+  Within the for loop above that iterates through all the rows,
+  create a variable called state
+  which will be the number of the data hour of the current div iteration.
+  This was attempted in line 88. Not sure if ok or how to test.
+  AND
+  create an if statement such that:
+  if (state === JSON.parse(moment().hour())), CHECK SYNTAX with Matthew
+  change the class to present
+  if (state < JSON.parse(moment().hour())),
+  change the class to past
+  else change the class to future
   */
 
-   //var testEl = document.querySelectorAll('[data-hour="9"]');
-   //testEl[0].innerHTML = "Does this target the correct row(s)?";//yes,
-   //so why can't I add text / change style, etc.
-   //Surely testEl can now be manipulated? Tried with id instead:
- // var testEl = $('#9');
-   //testEl.css('background-color', 'red');
-
 /*
-Lines 101 and 102 above would successfully change the inner html for the whole row.
-But I struggled to change anything else so switched to trying with ids 
-instead of data attributes.
-
-  Add something to the css to make the class of grey
- have background-color grey.
- Even though none of them have a class of grey at the moment.
-
-Access the data attributes you've already set up by using
-Look at Laura's way of doing this in activity 5 from last night
-jQuery equivalent of document.getElement methods to save those
-divs to a variable, as in something like:
-var targetDivs = document.getElement by class / jQuery equiv.
-
-When you have worked out how to target by data-hour,
- you can change the default green to grey or red as required:
-
-  if (state === JSON.parse(moment().hour())), make the area red
-  if (state < JSON.parse(moment().hour())), make the areas grey
-  else make the areas green / do nothing (because they already are green)
- }
- and I would think that the above 5 lines go into the for loop you started.
- */
- /*
 For changing style, check W7D1A1 or W7D1A7.
  */
  
